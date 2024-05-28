fdbb9ce8eca78c5b50dc2d5a5458e30486c632d5
brucelee03 <rohit.prajapati25603@gmail.com>
2024-05-28 08:51:05 +0000

    Did Some Changes

--- app.js
+++ app.js
@@ -26,28 +26,26 @@ initializeDBServer()
 
 //API 1
 // Senerio 1: Get a list of all todos whose status is 'TO DO'
-app.get("/todos/", async (request, response) => {
-  const hasPriorityAndStatusProperties = (requestQuery) => {
-  return (
-    requestQuery.priority !== undefined && requestQuery.status !== undefined
-  );
-  };
+app.get('/todos/', async (request, response) => {
+  const hasPriorityAndStatusProperties = requestQuery => {
+    return (
+      requestQuery.priority !== undefined && requestQuery.status !== undefined
+    )
+  }
 
-  const hasPriorityProperty = (requestQuery) => {
-    return requestQuery.priority !== undefined;
-  };
+  const hasPriorityProperty = requestQuery => {
+    return requestQuery.priority !== undefined
+  }
 
-  const hasStatusProperty = (requestQuery) => {
-    return requestQuery.status !== undefined;
-  };
- let data = null;
- let getTodosQuery = "";
- const { search_q = "", priority, status } = request.query;
+  const hasStatusProperty = requestQuery => {
+    return requestQuery.status !== undefined
+  }
+  let getTodosQuery = ''
+  const {search_q = '', priority, status} = request.query
 
-
- switch (true) {
-  case hasPriorityAndStatusProperties(request.query): //if this is true then below query is taken in the code
-   getTodosQuery = `
+  switch (true) {
+    case hasPriorityAndStatusProperties(request.query): //if this is true then below query is taken in the code
+      getTodosQuery = `
    SELECT
     *
    FROM
@@ -55,39 +53,38 @@ app.get("/todos/", async (request, response) => {
    WHERE
     todo LIKE '%${search_q}%'
     AND status = '${status}'
-    AND priority = '${priority}';`;
-   break;
-  case hasPriorityProperty(request.query):
-   getTodosQuery = `
+    AND priority = '${priority}';`
+      break
+    case hasPriorityProperty(request.query):
+      getTodosQuery = `
    SELECT
     *
    FROM
     todo 
    WHERE
     todo LIKE '%${search_q}%'
-    AND priority = '${priority}';`;
-   break;
-  case hasStatusProperty(request.query):
-   getTodosQuery = `
+    AND priority = '${priority}';`
+      break
+    case hasStatusProperty(request.query):
+      getTodosQuery = `
    SELECT
     *
    FROM
     todo 
    WHERE
     todo LIKE '%${search_q}%'
-    AND status = '${status}';`;
-   break;
-  default:
-   getTodosQuery = `
+    AND status = '${status}';`
+      break
+    default:
+      getTodosQuery = `
    SELECT
     *
    FROM
     todo 
    WHERE
-    todo LIKE '%${search_q}%';`;
- }
+    todo LIKE '%${search_q}%';`
+  }
 
-
- data = await database.all(getTodosQuery);
- response.send(data);
-});
+  const todoArray = await db.all(getTodosQuery)
+  response.send(todoArray)
+})
