28999354695847f3b621b02bc20e0ac5ecc9cc18
brucelee03 <rohit.prajapati25603@gmail.com>
2024-05-28 08:30:39 +0000

    Checking Erro

---
+++ app.js
@@ -0,0 +1,93 @@
+const express = require('express')
+const path = require('path')
+const {open} = require('sqlite')
+const sqlite3 = require('sqlite3')
+const app = express()
+app.use(express.json())
+
+const dbPath = path.join(__dirname, 'todoApplication.db')
+let db = null
+
+const initializeDBServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    })
+    app.listen(3000, () => {
+      console.log('Server is Running at http://localhost:3000')
+    })
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`)
+    process.exit(1)
+  }
+}
+initializeDBServer()
+
+//API 1
+// Senerio 1: Get a list of all todos whose status is 'TO DO'
+app.get("/todos/", async (request, response) => {
+  const hasPriorityAndStatusProperties = (requestQuery) => {
+  return (
+    requestQuery.priority !== undefined && requestQuery.status !== undefined
+  );
+  };
+
+  const hasPriorityProperty = (requestQuery) => {
+    return requestQuery.priority !== undefined;
+  };
+
+  const hasStatusProperty = (requestQuery) => {
+    return requestQuery.status !== undefined;
+  };
+ let data = null;
+ let getTodosQuery = "";
+ const { search_q = "", priority, status } = request.query;
+
+
+ switch (true) {
+  case hasPriorityAndStatusProperties(request.query): //if this is true then below query is taken in the code
+   getTodosQuery = `
+   SELECT
+    *
+   FROM
+    todo 
+   WHERE
+    todo LIKE '%${search_q}%'
+    AND status = '${status}'
+    AND priority = '${priority}';`;
+   break;
+  case hasPriorityProperty(request.query):
+   getTodosQuery = `
+   SELECT
+    *
+   FROM
+    todo 
+   WHERE
+    todo LIKE '%${search_q}%'
+    AND priority = '${priority}';`;
+   break;
+  case hasStatusProperty(request.query):
+   getTodosQuery = `
+   SELECT
+    *
+   FROM
+    todo 
+   WHERE
+    todo LIKE '%${search_q}%'
+    AND status = '${status}';`;
+   break;
+  default:
+   getTodosQuery = `
+   SELECT
+    *
+   FROM
+    todo 
+   WHERE
+    todo LIKE '%${search_q}%';`;
+ }
+
+
+ data = await database.all(getTodosQuery);
+ response.send(data);
+});
