de217c765958af41057f690a2188d131e44add55
brucelee03 <rohit.prajapati25603@gmail.com>
2024-05-29 06:58:54 +0000

    Fixed the Error & Completed the API

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const {open} = require('sqlite')
 const sqlite3 = require('sqlite3')
+
 const app = express()
 app.use(express.json())
 
@@ -23,10 +24,9 @@ const initializeDBServer = async () => {
   }
 }
 initializeDBServer()
-
-//API 1
-// Senerio 1: Get a list of all todos whose status is 'TO DO'
+// API 1
 app.get('/todos/', async (request, response) => {
+  const requestQuery = request.query
   const hasPriorityAndStatusProperties = requestQuery => {
     return (
       requestQuery.priority !== undefined && requestQuery.status !== undefined
@@ -40,51 +40,104 @@ app.get('/todos/', async (request, response) => {
   const hasStatusProperty = requestQuery => {
     return requestQuery.status !== undefined
   }
+
   let getTodosQuery = ''
   const {search_q = '', priority, status} = request.query
-
   switch (true) {
-    case hasPriorityAndStatusProperties(request.query): //if this is true then below query is taken in the code
+    case hasPriorityAndStatusProperties(request.query):
       getTodosQuery = `
-   SELECT
-    *
-   FROM
-    todo 
-   WHERE
-    todo LIKE '%${search_q}%'
-    AND status = '${status}'
-    AND priority = '${priority}';`
+      SELECT
+        *
+      FROM
+        todo 
+      WHERE
+        todo LIKE '%${search_q}%'
+        AND status = '${status}'
+        AND priority = '${priority}';`
       break
     case hasPriorityProperty(request.query):
       getTodosQuery = `
-   SELECT
-    *
-   FROM
-    todo 
-   WHERE
-    todo LIKE '%${search_q}%'
-    AND priority = '${priority}';`
+      SELECT
+        *
+      FROM
+        todo 
+      WHERE
+        todo LIKE '%${search_q}%'
+        AND priority = '${priority}';`
       break
     case hasStatusProperty(request.query):
       getTodosQuery = `
-   SELECT
-    *
-   FROM
-    todo 
-   WHERE
-    todo LIKE '%${search_q}%'
-    AND status = '${status}';`
+      SELECT
+        *
+      FROM
+        todo 
+      WHERE
+        todo LIKE '%${search_q}%'
+        AND status = '${status}';`
       break
     default:
       getTodosQuery = `
-   SELECT
-    *
-   FROM
-    todo 
-   WHERE
-    todo LIKE '%${search_q}%';`
+      SELECT
+        *
+      FROM
+        todo 
+      WHERE
+        todo LIKE '%${search_q}%';`
   }
-
-  const todoArray = await db.all(getTodosQuery)
-  response.send(todoArray)
+  const todos = await db.all(getTodosQuery)
+  response.send(todos)
 })
+
+// API 2
+app.get('/todos/:todoId/', async (request, response) => {
+  const {todoId} = request.params
+  const todo = await db.get(`SELECT * FROM todo WHERE id = ${todoId}`)
+  response.send(todo)
+})
+
+// API 3
+app.post('/todos/', async (request, response) => {
+  const {id, todo, priority, status} = request.body
+  const result = await db.run(`
+    INSERT INTO todo (id, todo, priority, status)
+    VALUES (${id}, '${todo}', '${priority}', '${status}')
+  `)
+  response.send(`Todo Successfully Added`)
+})
+
+// API 4
+app.put('/todos/:todoId/', async (request, response) => {
+  const {todoId} = request.params
+  const requestBody = request.body
+  let updateColumn = ''
+  switch (true) {
+    case requestBody.status !== undefined:
+      updateColumn = 'Status'
+      break
+    case requestBody.priority !== undefined:
+      updateColumn = 'Priority'
+      break
+    case requestBody.todo !== undefined:
+      updateColumn = 'Todo'
+      break
+  }
+  const priviousTodoQuery = `SELECT * FROM todo WHERE id = ${todoId};`
+  const priviousTodo = await db.get(priviousTodoQuery)
+  const {
+    todo = priviousTodo.todo,
+    priority = priviousTodo.priority,
+    status = priviousTodo.status,
+  } = request.body
+  const updateTodoQuery = `UPDATE todo SET todo='${todo}', priority='${priority}', status='${status}' WHERE id = ${todoId};`
+  await db.run(updateTodoQuery)
+  response.send(`${updateColumn} Updated`)
+})
+
+// API 5
+app.delete('/todos/:todoId/', async (request, response) => {
+  const {todoId} = request.params
+  const result = await db.run(`DELETE FROM todo WHERE id = ${todoId}`)
+  response.send(`Todo Deleted`)
+})
+
+module.exports = app
